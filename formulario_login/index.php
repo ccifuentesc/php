3c561e5f7649399c12fe2bdc329e2c1eab88f2b9
ccifuentesc <cavaliercifuentes@gmail.com>
2022-07-28 00:22:09 -0500

    Cambios

---
+++ formulario_login/index.php
@@ -0,0 +1,69 @@
+<?php
+
+ini_set('display_errors',1);
+ini_set('display_startup_errors',1);
+error_reporting(E_ALL);
+
+if($_POST){
+
+    $usuario = $_POST["txtUsuario"];
+    $clave = $_POST["txtClave"];
+
+    if($usuario == "admin" && $clave == "123456"){
+        header("Location: acceso-confirmado.php");
+    }
+    else{
+        $msg = "Usuario o clave incorrectos";
+    }
+
+}
+
+?>
+
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Inicio</title>
+    <!-- CSS only -->
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx" crossorigin="anonymous">
+</head>
+<body>
+    
+    <header>
+
+
+
+    </header>
+
+    <main class="container">
+        <div class="row">
+            <div class="col-12 text-center">
+                <h1>Formulario</h1>
+            </div>
+        </div>
+
+        <div class="row">
+            <div class="col-12">
+                <form action="" method="POST">
+                    <div class="py-2">
+                        <?php if(isset($msg)){$msg;} ?>
+                        <label for="txtUsuario">Usuario:</label>
+                        <input class="form-control" type="text" name="txtUsuario">
+                    </div>
+                    <div class="py-2">
+                        <label for="txtClave">Clave: </label>
+                        <input class="form-control" type="password" name="txtClave" id="">
+                    </div>
+                    <div class="py-3">
+                        <button class="btn btn-primary" type="submit">Ingresar</button>
+                    </div>
+                </form>
+            </div>
+        </div>
+    </main>
+
+</body>
+</html>
