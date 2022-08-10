096e687a011b5f7890aaf134314433ede33e73a3
ccifuentesc <cavaliercifuentes@gmail.com>
2022-08-10 01:54:13 -0500

    Changes

--- calcular_iva.php
+++ calcular_iva.php
@@ -30,6 +30,7 @@ if ($_POST) {
 
 }
 
+
 ?>
 
 <!DOCTYPE html>
@@ -41,6 +42,7 @@ if ($_POST) {
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Calculadora de IVA</title>
     <!-- CSS only -->
+
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx" crossorigin="anonymous">
 </head>
 
@@ -55,7 +57,9 @@ if ($_POST) {
         </div>
         <div class="row">
             <div class="col-3 offset-2">
-                <form method="POST">
+           
+           
+            <form method="POST">
                     <div class="pb-3">
                         <label for="">IVA</label>
                     </div>
